c452d8733083f32244ed74bcff981e3e70a92f25
dantemessy <thepunishar1@gmail.com>
2019-11-14 19:35:58 +0200

    answer q5

--- app.js
+++ app.js
@@ -115,17 +115,32 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+   var matrix = [];
+   var answer = multiply(multiply(multArr[0],multArr[1])[0],multArr[2])[0];
+   matrix.push(answer);
+   matrix.push("The numbers 2,3,4 have a product of 24.");
+   //console.log(matrix);
+   return matrix ;
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
 // Don't forget to create a new branch for your work on the next question, if you attempt it.
+
+
+
+
+
+
+
+
+
 
 /////////////////////////////////////
 /* STRETCH GOAL: Problem 6
