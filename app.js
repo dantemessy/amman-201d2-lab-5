19f54ec676c07d03f8905fb35252110e0dd2748b
dantemessy <thepunishar1@gmail.com>
2019-11-14 18:28:57 +0200

    solve q3

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ function sum(a, b) { //eslint-disable-line
    result.push(sum);
    result.push("The sum of 4 and 7 is 11.");
    return result;
+
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -57,11 +58,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+   var summing = sum(sum(a,b)[0],c)[0];
+   var multiplying = multiply(multiply(a, b)[0],c)[0];
+   var result = [];
+   result.push(summing);
+   result.push(multiplying);
+   result.push("4 and 7 and 5 sum to 16.");
+   result.push("The product of 4 and 7 and 5 is 140."); //console.log(result)
+   return result;
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
